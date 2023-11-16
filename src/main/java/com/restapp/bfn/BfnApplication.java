35ab7f552b65c1806a563793de49f4f6df53d23e
Stefan Zabrian <faniss2005@yahoo.com>
2023-11-16 17:42:49 +0200

    Initial Commit < Creation of Project < > DataBase Config > Web Security Config

---
+++ src/main/java/com/restapp/bfn/BfnApplication.java
@@ -0,0 +1,13 @@
+package com.restapp.bfn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapp/bfn/BfnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapp.bfn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
